d1c5c647606d417437cb1962af4629ef4fc03966
diazjuanjo <diazjuanjose@icloud.com>
2022-10-22 00:10:49 -0300

    hasta clase 103

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Pressable} from 'react-native';
 
 const Paciente = ({item}) => {
   const {paciente, fecha} = item;
@@ -15,11 +15,66 @@ const Paciente = ({item}) => {
     return nuevaFecha.toLocaleDateString('es-ES', opciones);
   };
   return (
-    <View>
-      <Text>{paciente}</Text>
-      <Text>{formatearFecha(fecha)}</Text>
+    <View style={styles.contenedor}>
+      <Text style={styles.label}>Paciente</Text>
+      <Text style={styles.texto}>{paciente}</Text>
+      <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
+      <View style={styles.contenedorBotones}>
+        <Pressable style={[styles.btn, styles.btnEditar]}>
+          <Text style={styles.btnTexto}>Editar</Text>
+        </Pressable>
+        <Pressable style={[styles.btn, styles.btnEliminar]}>
+          <Text style={styles.btnTexto}>Eliminar</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  contenedor:{
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderBottomColor: '#94a3B8',
+    borderBottomWidth: 1
+  },
+  label: {
+    color: '#374151',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    marginBottom: 10
+  },
+  texto: {
+    color: '#6D28D9',
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 10
+  },
+  fecha: {
+    color: '#374151'
+  },
+  contenedorBotones:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20
+  },
+  btn: {
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 5
+  },
+  btnEditar: {
+    backgroundColor: '#F59E0B'
+  },
+  btnEliminar: {
+    backgroundColor: '#EF4444'
+  },
+  btnTexto: {
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    fontSize: 12,
+    color: '#FFF'
+  },
+})
 
 export default Paciente;
